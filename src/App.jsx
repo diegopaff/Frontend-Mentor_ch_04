8ab906188a0908dc3a6195c692a354d145adb7ca
paff <diego.curutchet89@gmail.com>
2023-03-30 17:21:30 -0300

    Services section in mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from '../src/sections/Header'
 import About from '../src/sections/About'
 import './App.scss'
+import Services from './sections/Services'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <About />
+      <Services />
     </div>
   )
 }
---
+++ src/sections/Services.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import '../scss/sections/_services.scss'
+
+const Services = () => {
+  return (
+    <div id='services' className='services-container'>
+        <div className="graphic-design service">
+            <h2 className="service__title">Graphic Design</h2>
+            <p className="service__description">Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients attention.</p>
+        </div>
+        <div className="photography service">
+            <h2 className="service__title">Photography</h2>
+            <p className="service__description">Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Services
